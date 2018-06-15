d057e56823319f767106ef3a0f5c9dbe5c335d2a
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2018-06-15 15:47:19 +0200

    Pyspark Script To Find The Number of Email Transactions Grouped Year Wise

--- processData.py
+++ processData.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python3
+
+import pyspark as pys
+sparkC = pys.SparkContext()
+
 from pyspark.sql import SparkSession
 
-spark = SparkSession \
-    .builder \
-    .master("spark://192.168.1.12:7077")\
-    .appName("LDSA Project") \
-    .config("spark.some.config.option", "some-value") \
-    .getOrCreate()
+sparkS = SparkSession.builder.appName("LDSA Email Analytics").config("spark.some.config.option", "some-value").getOrCreate()
 
-df=spark.read.format("csv").option("header","false").option("delimiter", "\t")\
-    .load("/home/ubuntu/DATA/export.tsv")
+inputDataframe=sparkS.read.format("csv").option("header", "false").option("delimiter", "\t").load("file:///home/ubuntu/export.tsv")
+inputDataframe.createOrReplaceTempView("Enron_initial")
 
-df.createOrReplaceTempView("Enron")
+intermediateDataframe = sparkS.sql("SELECT _c0 as messageIds, concat(split(_c2, ' ')[3]) as Year FROM Enron_initial where _c2 like '%:%' order by Year asc")
+intermediateDataframe.createOrReplaceTempView("Enron_intermediate")
 
-sqlDF = spark.sql("SELECT _c1 as subject, concat(split(_c2, ' ')[3],'-',split(_c2, ' ')[2],'-', split(_c2, ' ')[1], ' ') as date1 FROM Enron where _c2 like '%:%' order by date1 asc")
+outputDataframe = sparkS.sql("SELECT count(messageIds) as No_of_Messages, Year as Year from Enron_intermediate group by Year").take(7)
+
+with open('Counts.txt', 'w') as outputFile:
+    for row in outputDataframe:
+        outputFile.write(str(row) + "\n")
